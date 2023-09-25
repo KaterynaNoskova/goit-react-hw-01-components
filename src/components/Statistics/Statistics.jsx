4b97dc3232177e0409d91f256ed91c619423d9fa
KaterynaNoskova <kateryna.noskova@gmail.com>
2023-09-25 16:44:57 +0200

    fixed HW

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,13 +3,13 @@ import css from './Statistics.module.css';
 export const Statistics = ({ title, statistics }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
-        {statistics.map(({id, label, percentage}) => (
-            <li className={css.item} key={id}>
-          <span className={css.statsLabel}>{label}</span>
-          <span className={css.percentage}>{percentage}%</span>
-        </li>
+        {statistics.map(({ id, label, percentage }) => (
+          <li className={css.item} key={id}>
+            <span className={css.statsLabel}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
         ))}
       </ul>
     </section>
